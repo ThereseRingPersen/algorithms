db3b08d02e392a45b78445a475685b861435d525
Therese Persen <therese.persen@me.com>
2020-07-28 17:00:59 +0200

    tests for removing nodes in the tree.

--- src/bstRecursive/test.js
+++ src/bstRecursive/test.js
@@ -23,6 +23,49 @@ describe("Tree", () => {
   test("Tree can insert a node", () => {
     const tree = new Tree();
     tree.insert("a");
-    expect(tree.root).toEqual("a");
+    expect(tree.root.data).toEqual("a");
+  });
+  test("Can remove a node with two children from a tree", () => {
+    const tree = new Tree();
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(55);
+    tree.insert(53);
+    tree.insert(56);
+    tree.insert(100);
+    tree.insert(70);
+    tree.insert(69);
+    tree.insert(90);
+    tree.insert(120);
+    tree.remove(100);
+    expect(tree.root.right.right.data).toEqual(120);
+    expect(tree.root.right.right.left.data).toEqual(70);
+    expect(tree.root.right.right.right).toEqual(null);
+  });
+  test("Can remove a leafnode from a tree", () => {
+    const tree = new Tree();
+    tree.insert(40);
+    tree.insert(60);
+    tree.remove(60);
+    expect(tree.root.right).toEqual(null);
+  });
+  test("Can remove a node with one right child from a tree", () => {
+    const tree = new Tree();
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(50);
+    tree.remove(60);
+    expect(tree.root.right.right).toEqual(null);
+    expect(tree.root.right.data).toEqual(50);
+  });
+
+  test("Can remove a node with one left child from a tree", () => {
+    const tree = new Tree();
+    tree.insert(40);
+    tree.insert(30);
+    tree.insert(20);
+    tree.remove(30);
+    expect(tree.root.left.left).toEqual(null);
+    expect(tree.root.left.data).toEqual(20);
   });
 });
