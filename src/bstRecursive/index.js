8ddaa1c5d3c7762c2de2f7794bef8814695739ff
Therese Persen <therese.persen@me.com>
2020-07-28 17:00:16 +0200

    Tree method delete and insert

--- src/bstRecursive/index.js
+++ src/bstRecursive/index.js
@@ -11,7 +11,7 @@ class Tree {
   }
   insert(data) {
     const newNode = new Node(data);
-    if (root === null) {
+    if (this.root === null) {
       this.root = newNode;
     } else {
       this.insertNode(this.root, newNode);
@@ -32,4 +32,37 @@ class Tree {
       }
     }
   }
+  remove(data) {
+    this.root = this.removeNode(this.root, data);
+  }
+  removeNode(node, data) {
+    if (node === null) {
+      return null;
+    } else if (node.data > data) {
+      node.left = this.removeNode(node.left, data);
+    } else if (node.data < data) {
+      node.right = this.removeNode(node.right, data);
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+      } else if (node.left === null) {
+        node = node.right;
+      } else if (node.right === null) {
+        node = node.left;
+      } else {
+        const minNode = this.findMinNode(node.right);
+        node.data = minNode.data;
+        node.right = this.removeNode(node.right, minNode.data);
+      }
+    }
+    return node;
+  }
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
 }
+module.exports = { Node, Tree };
