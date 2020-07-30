b5152ef0a0d6d282cf3abd25a14d4ffad618a6d5
Therese Persen <therese.persen@me.com>
2020-07-30 11:59:59 +0200

    add space between code execution and expect to add to readability

--- src/bstRecursive/test.js
+++ src/bstRecursive/test.js
@@ -9,6 +9,7 @@ describe("Node", () => {
 
   test("Node has a data and children properties", () => {
     const n = new Node("a");
+
     expect(n.data).toEqual("a");
     expect(n.left).toEqual(null);
     expect(n.right).toEqual(null);
@@ -18,26 +19,30 @@ describe("Node", () => {
 describe("Tree insert", () => {
   test("starts empty", () => {
     const tree = new Tree();
+
     expect(tree.root).toEqual(null);
   });
 
   test("Tree can insert a node", () => {
     const tree = new Tree();
     tree.insert("a");
+
     expect(tree.root.data).toEqual("a");
   });
 });
 
-describe("Tree find.", () => {
+describe("Tree find", () => { 
   test("Returns node when it exists.", () => {
     const tree = buildTree();
     const node = tree.find(40);
+
     expect(node.data).toEqual(40);
   });
 
   test("Returns null when the value does not exist.", () => {
     const tree = buildTree();
     const node = tree.find(12);
+
     expect(node).toEqual(null);
   });
 });
@@ -61,6 +66,7 @@ describe("Tree remove", () => {
     tree.insert(90);
     tree.insert(120);
     tree.remove(100);
+
     expect(tree.root.right.right.data).toEqual(120);
     expect(tree.root.right.right.left.data).toEqual(70);
     expect(tree.root.right.right.right).toEqual(null);
@@ -80,6 +86,7 @@ describe("Tree remove", () => {
     tree.insert(60);
     tree.insert(50);
     tree.remove(60);
+
     expect(tree.root.right.right).toEqual(null);
     expect(tree.root.right.data).toEqual(50);
   });
@@ -90,6 +97,7 @@ describe("Tree remove", () => {
     tree.insert(30);
     tree.insert(20);
     tree.remove(30);
+
     expect(tree.root.left.left).toEqual(null);
     expect(tree.root.left.data).toEqual(20);
   });
@@ -107,6 +115,7 @@ describe("Tree traveral", () => {
   test("Inorder traversal", () => {
     const tree = buildTree();
     tree.inorder(tree.root);
+
     expect(console.log.mock.calls[0][0]).toEqual(20);
     expect(console.log.mock.calls[1][0]).toEqual(30);
     expect(console.log.mock.calls[2][0]).toEqual(35);
@@ -118,6 +127,7 @@ describe("Tree traveral", () => {
   test("Preorder traversal", () => {
     const tree = buildTree();
     tree.preorder(tree.root);
+
     expect(console.log.mock.calls[0][0]).toEqual(40);
     expect(console.log.mock.calls[1][0]).toEqual(30);
     expect(console.log.mock.calls[2][0]).toEqual(20);
@@ -129,6 +139,7 @@ describe("Tree traveral", () => {
   test("Postorder traversal", () => {
     const tree = buildTree();
     tree.postorder(tree.root);
+    
     expect(console.log.mock.calls[0][0]).toEqual(20);
     expect(console.log.mock.calls[1][0]).toEqual(35);
     expect(console.log.mock.calls[2][0]).toEqual(30);
